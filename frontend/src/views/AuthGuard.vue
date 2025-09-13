d6bb4e51eb3375ef4120ba0e4681a9eb5734871f
QiHuang02 <128583144+QiHuang02@users.noreply.github.com>
2025-09-13 15:43:47 +0800

    feat: add auth guard view

---
+++ frontend/src/views/AuthGuard.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-result
+    icon="warning"
+    title="Insufficient permissions"
+    sub-title="You do not have access to view this page."
+  />
+</template>
+
+<script setup>
+// This view is used when a user navigates to a route
+// they do not have sufficient permissions to access.
+</script>
